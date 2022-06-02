3a7ca2a36a5444536014fb1a33be4ae3d42a2247
yogesh.bhattarai <yogesh.bhattarai@aitcentre.com>
2022-06-02 13:32:09 +0545

    git fetched data from the api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+
+  useEffect(() => fetchData(), []);
+
+  const fetchData = () => {
+    fetch("https://randomuser.me/api?results=30")
+      .then((res) => res.json())
+      .then((res) => setData(res.results));
+  };
+  return <div className="App">{data ? <></> : <h1>Loading..</h1>}</div>;
 }
 
 export default App;
